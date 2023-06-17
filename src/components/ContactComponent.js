ad753b6f81a2e150f09be9b36389ee54925be6a2
trandtrieu <114125026+trandtrieu@users.noreply.github.com>
2023-06-18 00:05:00 +0700

    controledd form validation

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -8,6 +8,8 @@ import {
   Label,
   Input,
   Col,
+  Row,
+  FormFeedback,
 } from "reactstrap";
 import { Link } from "react-router-dom";
 class Contact extends Component {
@@ -22,10 +24,54 @@ class Contact extends Component {
       agree: false,
       contactType: "Tel.",
       message: "",
+      touched: {
+        firstname: false,
+        lastname: false,
+        telnum: false,
+        email: false,
+      },
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
+  }
+
+  handleBlur = (field) => (evt) => {
+    this.setState({
+      touched: { ...this.state.touched, [field]: true },
+    });
+  };
+
+  validate(firstname, lastname, telnum, email) {
+    const errors = {
+      firstname: "",
+      lastname: "",
+      telnum: "",
+      email: "",
+    };
+
+    if (this.state.touched.firstname && firstname.length < 3)
+      errors.firstname = "First Name should be >= 3 characters";
+    else if (this.state.touched.firstname && firstname.length > 10)
+      errors.firstname = "First Name should be <= 10 characters";
+
+    if (this.state.touched.lastname && lastname.length < 3)
+      errors.lastname = "Last Name should be >= 3 characters";
+    else if (this.state.touched.lastname && lastname.length > 10)
+      errors.lastname = "Last Name should be <= 10 characters";
+
+    const reg = /^\d+$/;
+    if (this.state.touched.telnum && !reg.test(telnum))
+      errors.telnum = "Tel. Number should contain only numbers";
+
+    if (
+      this.state.touched.email &&
+      email.split("").filter((x) => x === "@").length !== 1
+    )
+      errors.email = "Email should contain a @";
+
+    return errors;
   }
 
   handleInputChange(event) {
@@ -45,6 +91,12 @@ class Contact extends Component {
   }
 
   render() {
+    const errors = this.validate(
+      this.state.firstname,
+      this.state.lastname,
+      this.state.telnum,
+      this.state.email
+    );
     return (
       <div className="container">
         <div className="row">
@@ -122,6 +174,9 @@ class Contact extends Component {
                       name="firstname"
                       placeholder="First Name"
                       value={this.state.firstname}
+                      valid={errors.firstname === ""}
+                      invalid={errors.firstname !== ""}
+                      onBlur={this.handleBlur("firstname")}
                       onChange={this.handleInputChange}
                     />
                   </Col>
@@ -137,6 +192,9 @@ class Contact extends Component {
                       name="lastname"
                       placeholder="Last Name"
                       value={this.state.lastname}
+                      valid={errors.lastname === ""}
+                      invalid={errors.lastname !== ""}
+                      onBlur={this.handleBlur("lastname")}
                       onChange={this.handleInputChange}
                     />
                   </Col>
@@ -152,6 +210,9 @@ class Contact extends Component {
                       name="telnum"
                       placeholder="Tel. number"
                       value={this.state.telnum}
+                      valid={errors.telnum === ""}
+                      invalid={errors.telnum !== ""}
+                      onBlur={this.handleBlur("telnum")}
                       onChange={this.handleInputChange}
                     />
                   </Col>
@@ -167,6 +228,9 @@ class Contact extends Component {
                       name="email"
                       placeholder="Email"
                       value={this.state.email}
+                      valid={errors.email === ""}
+                      invalid={errors.email !== ""}
+                      onBlur={this.handleBlur("email")}
                       onChange={this.handleInputChange}
                     />
                   </Col>
